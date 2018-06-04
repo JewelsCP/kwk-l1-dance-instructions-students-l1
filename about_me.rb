a7465f3836f92cbbe80215be5198303cb99b9315
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 22:31:18 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,7 +1,4 @@
-local_greeting = "Hey, dude"
-your_name = "Randy"
-my_name = "Ame"
-time_of_day = "summer time"
+
 
 def greeting
   puts "#{local-greetings},I'm #{your_name}, I'm #{my_name}. How's your #{time_of_day}?"
@@ -11,4 +8,5 @@ local_greeting = "Hey, dude"
 your_name = "Randy"
 my_name = "Ame"
 time_of_day = "summer time"
+
 greeting
