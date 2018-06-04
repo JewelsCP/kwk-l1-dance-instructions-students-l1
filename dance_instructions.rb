5a7ccbbbacdaa6925cc38d2ce8ea6b566c335d73
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 22:14:06 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -26,4 +26,5 @@ end
 def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
-  puts ""
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
+end
