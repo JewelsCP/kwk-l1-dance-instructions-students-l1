c88a0647cb591fc118fd6fd2d010f72404becb60
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 22:07:29 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,12 +1,12 @@
 
 # Code your methods below
 
-def Gangnam_Style
+def lower_body
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
   puts "Lift right foot"
   puts "Return right foot to the ground"
   puts "Finishing with a small skip-step backward"
-  
+ end 
   def skip-step_left
     puts "Lower left foot to the ground"
     puts "Bounce left foot back up slightly, kicking it a few inches back"
