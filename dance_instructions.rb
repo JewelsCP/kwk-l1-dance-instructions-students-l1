84b299d21b805a253a9e0790c5fc0a67f0e2f796
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 22:13:14 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,20 +1,27 @@
 
 # Code your methods below
 
-def lower_body
+def stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
+ end
+
+def base_footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
   puts "Finishing with a small skip-step backward"
  end 
-  def skip-step_left
-    puts "Lower left foot to the ground"
-    puts "Bounce left foot back up slightly, kicking it a few inches back"
-    puts "Left, Right, Left and Left"
-    
-    def skip-step_right
-      puts "Left, Right, Left and Left"
-      puts "Bounce right foot back up slightly, kicking it a few inches back"
-      puts "Right, Left, Right and Right"
-      
   
+def skip-step_left
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+end 
+  
+def skip-step_right
+  puts "Left, Right, Left and Left"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+end 
+  
+def bob_the_reins
+  puts ""
