3f561d7e2b2518baee77033731f3867a0b229a21
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 22:29:00 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,8 +1,10 @@
 local_greeting =
-
+your_name = 
+my_name =
+time_of_day = 
 
 def greeting
-  puts "Hi, Chen, I'm Sara Beth. How's your afternoon?"
+  puts "#{local-greetings},I'm #{your_name}, I'm #{my_name}. How's your afternoon?"
 end
 
 greeting
